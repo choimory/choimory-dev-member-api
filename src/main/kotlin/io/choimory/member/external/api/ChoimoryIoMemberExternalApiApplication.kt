7c2fa14bde411c185ba8bcdec869a3293c5e810f
choimory <choimory@naver.com>
2025-07-02 20:45:36 +0900

    Initial commit

---
+++ src/main/kotlin/io/choimory/member/external/api/ChoimoryIoMemberExternalApiApplication.kt
@@ -0,0 +1,11 @@
+package io.choimory.member.external.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChoimoryIoMemberExternalApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChoimoryIoMemberExternalApiApplication>(*args)
+}
---
+++ src/test/kotlin/io/choimory/member/external/api/TestChoimoryIoMemberExternalApiApplication.kt
@@ -0,0 +1,9 @@
+package io.choimory.member.external.api
+
+import org.springframework.boot.fromApplication
+import org.springframework.boot.with
+
+
+fun main(args: Array<String>) {
+    fromApplication<ChoimoryIoMemberExternalApiApplication>().with(TestcontainersConfiguration::class).run(*args)
+}
