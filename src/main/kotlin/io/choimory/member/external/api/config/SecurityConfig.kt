cb49a4253675bd1b0c1a0f260fccb4512172ba8e
yoonjoong <choimory@naver.com>
2025-07-03 13:17:38 +0900

    fix: basic security off

--- src/main/kotlin/io/choimory/member/external/api/config/SecurityConfig.kt
+++ src/main/kotlin/io/choimory/member/external/api/config/SecurityConfig.kt
@@ -1,3 +1,23 @@
 package io.choimory.member.external.api.config
 
-class SecurityConfig
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+    @Bean
+    fun config(http: HttpSecurity): SecurityFilterChain {
+        http.csrf { it.disable() }
+            .cors { it.disable() }
+            .formLogin { it.disable() }
+            .logout { it.disable() }
+            .httpBasic { it.disable() }
+            .authorizeHttpRequests { it.anyRequest().permitAll() }
+
+        return http.build()
+    }
+}
