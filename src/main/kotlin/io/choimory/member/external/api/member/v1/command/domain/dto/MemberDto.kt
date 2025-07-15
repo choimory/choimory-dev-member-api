abfcb752de1d839b7cb5846e1b5e5ab2165d3997
yoonjoong <choimory@naver.com>
2025-07-15 16:17:58 +0900

    feat: member dto

--- src/main/kotlin/io/choimory/member/external/api/member/v1/command/domain/dto/MemberDto.kt
+++ src/main/kotlin/io/choimory/member/external/api/member/v1/command/domain/dto/MemberDto.kt
@@ -1,3 +1,9 @@
 package io.choimory.member.external.api.member.v1.command.domain.dto
 
-class MemberDto
+data class MemberDto(
+    val id: Long? = null,
+    val email: String? = null,
+    val nickname: String? = null,
+    val password: String? = null,
+    val introduce: String? = null,
+)
