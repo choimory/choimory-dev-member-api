383e64bcaf1c5e85253613f2afa9ed9e5038b672
yoonjoong <choimory@naver.com>
2025-07-15 16:36:27 +0900

    feat: Common Exception Handling

--- src/main/kotlin/io/choimory/member/external/api/common/exception/CommonExceptionHandler.kt
+++ src/main/kotlin/io/choimory/member/external/api/common/exception/CommonExceptionHandler.kt
@@ -4,49 +4,93 @@ import io.choimory.member.external.api.common.domain.dto.CommonValidateResult
 import io.choimory.member.external.api.common.domain.response.CommonResponse
 import jakarta.validation.ConstraintViolationException
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.security.core.AuthenticationException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import java.util.stream.Collectors
 
 @RestControllerAdvice("io.choimory")
 class CommonExceptionHandler {
     @ExceptionHandler(Exception::class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    fun exception(): CommonResponse<String> {
-        TODO()
+    fun exception(e: Exception): CommonResponse<String> {
+        e.printStackTrace()
+        return CommonResponse(
+            HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            HttpStatus.INTERNAL_SERVER_ERROR.reasonPhrase,
+            e.message,
+        )
     }
 
     @ExceptionHandler(RuntimeException::class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    fun runtimeException(): CommonResponse<String> {
-        TODO()
+    fun runtimeException(e: RuntimeException): CommonResponse<String> {
+        e.printStackTrace()
+        return CommonResponse(
+            HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            HttpStatus.INTERNAL_SERVER_ERROR.reasonPhrase,
+            e.message,
+        )
     }
 
     @ExceptionHandler(CommonException::class)
-    fun commonException(): CommonResponse<String> {
-        TODO()
+    fun commonException(e: CommonException): ResponseEntity<CommonResponse<String>> {
+        return ResponseEntity(
+            CommonResponse(
+                e.status.value(),
+                e.msg,
+            ),
+            e.status,
+        )
     }
 
     // Spring validation
     @ExceptionHandler(MethodArgumentNotValidException::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    fun methodArgumentNotValidException(): CommonResponse<List<CommonValidateResult>> {
-        TODO()
+    fun methodArgumentNotValidException(e: MethodArgumentNotValidException): CommonResponse<List<CommonValidateResult>> {
+        return CommonResponse(
+            HttpStatus.BAD_REQUEST.value(),
+            HttpStatus.BAD_REQUEST.reasonPhrase,
+            e.fieldErrors.stream()
+                .map { f ->
+                    CommonValidateResult(
+                        f.field,
+                        f.rejectedValue,
+                        f.defaultMessage,
+                    )
+                }.collect(Collectors.toUnmodifiableList()),
+        )
     }
 
     // Spring validation
     @ExceptionHandler(ConstraintViolationException::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    fun constraintViolationException(): CommonResponse<List<CommonValidateResult>> {
-        TODO()
+    fun constraintViolationException(e: ConstraintViolationException): CommonResponse<List<CommonValidateResult>> {
+        return CommonResponse(
+            HttpStatus.BAD_REQUEST.value(),
+            HttpStatus.BAD_REQUEST.reasonPhrase,
+            e.constraintViolations.stream()
+                .map { v ->
+                    CommonValidateResult(
+                        v.propertyPath.toString(),
+                        v.invalidValue,
+                        v.message,
+                    )
+                }.collect(Collectors.toUnmodifiableList()),
+        )
     }
 
     // Spring security
     @ExceptionHandler(AuthenticationException::class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    fun authenticateException() {
-        TODO()
+    fun authenticateException(e: Exception): CommonResponse<String> {
+        return CommonResponse(
+            HttpStatus.UNAUTHORIZED.value(),
+            HttpStatus.UNAUTHORIZED.reasonPhrase,
+            e.message,
+        )
     }
 }
