31f8a4902c771c88504a07d7a71126e457faa8f2
yoonjoong <choimory@naver.com>
2025-07-18 17:31:00 +0900

    fix: req, res change to data class

--- src/main/kotlin/io/choimory/member/external/api/member/v1/command/domain/request/CreateMemberRequest.kt
+++ src/main/kotlin/io/choimory/member/external/api/member/v1/command/domain/request/CreateMemberRequest.kt
@@ -2,7 +2,7 @@ package io.choimory.member.external.api.member.v1.command.domain.request
 
 import jakarta.validation.constraints.NotBlank
 
-class CreateMemberRequest(
+data class CreateMemberRequest(
     @NotBlank
     val password: String,
 )
--- src/main/kotlin/io/choimory/member/external/api/member/v1/command/domain/request/VerifyMemberRequest.kt
+++ src/main/kotlin/io/choimory/member/external/api/member/v1/command/domain/request/VerifyMemberRequest.kt
@@ -1,6 +1,6 @@
 package io.choimory.member.external.api.member.v1.command.domain.request
 
-class VerifyMemberRequest(
+data class VerifyMemberRequest(
     val email: String,
     val verifyCode: Int,
 )
