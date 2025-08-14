6c0b29a7b027aac85d1b11a59aef258756f9f1be
yoonjoong <choimory@naver.com>
2025-08-14 09:37:58 +0900

    fix: 응답의 코드를 코드와 코드명으로 분리

--- src/main/kotlin/dev/choimory/member/api/common/exception/CommonException.kt
+++ src/main/kotlin/dev/choimory/member/api/common/exception/CommonException.kt
@@ -3,7 +3,8 @@ package dev.choimory.member.api.common.exception
 import org.springframework.http.HttpStatus
 
 class CommonException(
-    val status: HttpStatus,
-    val code: Int? = status.value(),
-    val msg: String? = status.reasonPhrase,
+    val status: HttpStatus, // e.g. HttpStatus.OK
+    val code: Int? = status.value(), // e.g. 200
+    val codeName: String? = status.name, // e.g. OK
+    val msg: String? = status.reasonPhrase, // e.g. 성공하였습니다
 ) : RuntimeException()
